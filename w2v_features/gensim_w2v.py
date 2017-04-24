991e33840f032132cc5ec40ed4dccb2199546e9b
Nadheesh <nadheeshj@gmail.com>
2017-04-24 23:31:33 +0530

    improving the script

--- w2v_features/gensim_w2v.py
+++ w2v_features/gensim_w2v.py
@@ -9,7 +9,7 @@ from gensim.models import word2vec
 def load_data_for_w2v(data_path):
     df = pd.read_csv(data_path, encoding='latin1')
     text = df.review_text.unique()
-    cleaned_text = [dh.clean_reviews(sent) for sent in text]
+    cleaned_text = [dh.clean_reviews(sent, remove_stop_words=False) for sent in text]
 
     empty_reviews = [i for i, x in enumerate(cleaned_text) if x == ""]
 
@@ -19,13 +19,7 @@ def load_data_for_w2v(data_path):
     return cleaned_text
 
 
-def get_w2v_model(data_path="data/w2v/train1.csv", size=50, window=10, min_count=1, workers=2):
-    text = load_data_for_w2v(data_path)
-
-    lst = []
-    for t in text:
-        lst += [t.split()]
-
+def get_w2v_model(data_path="../data/w2v/train1.csv", size=50, window=10, min_count=1, workers=2):
     model_dir = 'models'
     model_name = "{:d}features_{:d}minwords_{:d}context".format(size, min_count, window)
     model_name = join(model_dir, model_name)
@@ -34,6 +28,12 @@ def get_w2v_model(data_path="data/w2v/train1.csv", size=50, window=10, min_count
         print('Load existing Word2Vec model \'%s\'' % split(model_name)[-1])
 
     else:
+        text = load_data_for_w2v(data_path)
+
+        lst = []
+        for t in text:
+            lst += [t.split()]
+
         model = word2vec.Word2Vec(lst, size=size, window=window, min_count=min_count, workers=workers)
 
         if not exists(model_dir):
@@ -44,12 +44,15 @@ def get_w2v_model(data_path="data/w2v/train1.csv", size=50, window=10, min_count
     return model
 
 
-def load_data_for_cnn(train_data_path, test_data_path, max_sequence_length=100, w2v_train_data=None, size=50, window=10, min_count=1,):
+def load_data_for_cnn(train_data_path, test_data_path, max_sequence_length=100, w2v_train_data=None, size=50, window=10,
+                      min_count=1, ):
     train_x, train_y = dh.load_data_multilabel(train_data_path)
     test_x, test_y = dh.load_data_multilabel(test_data_path)
+
     train_x_padded = dh.pad_sentences(train_x,
                                       max_length=max_sequence_length)  # required to emmit 0 as the pad key word
     test_x_padded = dh.pad_sentences(test_x, max_length=max_sequence_length)  # required to emmit 0 as the pad key word
+
     vocabulary, vocabulary_inv = dh.build_vocab(train_x_padded + test_x_padded)
 
     model = get_w2v_model(size=size, window=window, min_count=min_count)
@@ -65,8 +68,9 @@ def load_data_for_cnn(train_data_path, test_data_path, max_sequence_length=100, 
     return train_wv, train_y, test_wv, test_y
 
 
-def load_data_for_eval(test_data_path, max_sequence_length=100, w2v_train_data=None, size=50, window=10, min_count=1,):
+def load_data_for_eval(test_data_path, max_sequence_length=100, w2v_train_data=None, size=50, window=10, min_count=1, ):
     test_x, test_y = dh.load_data_multilabel(test_data_path)
+
     test_x_padded = dh.pad_sentences(test_x, max_length=max_sequence_length)  # required to emmit 0 as the pad key word
     vocabulary, vocabulary_inv = dh.build_vocab(test_x_padded)
 
